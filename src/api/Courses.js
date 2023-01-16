eed61804a6c6a34b9549f6ce097ebfe452e825a8
harjinder <49772260+raisingh22@users.noreply.github.com>
2023-01-16 18:52:37 +0530

    updated react native

---
+++ src/api/Courses.js
@@ -0,0 +1,64 @@
+const Courses = [
+    {
+        id: "1",
+        title: "Web Development",
+        image: require('../../assets/w.jpg'),
+        discription:
+            "This component must wrap all navigators structure. Usually, we'd render this component at the root of our app, which is usually the component exported from ",
+        course1: 'HTML',
+        course2: "CSS",
+        Course3: "JavaScript",
+    },
+    {
+        id: "2",
+        title: "Web Development",
+        image: require('../../assets/a.jpg'),
+        discription:
+            "This component must wrap all navigators structure. Usually, we'd render this component at the root of our app, which is usually the component exported from ",
+        course1: 'HTML',
+        course2: "CSS",
+        Course3: "JavaScript",
+    },
+    {
+        id: "3",
+        title: "Web Development",
+        image: require('../../assets/a.jpg'),
+        discription:
+            "This component must wrap all navigators structure. Usually, we'd render this component at the root of our app, which is usually the component exported from ",
+        course1: 'HTML',
+        course2: "CSS",
+        Course3: "JavaScript",
+    },
+    {
+        id: "4",
+        title: "Web Development",
+        image: require('../../assets/a.jpg'),
+        discription:
+            "This component must wrap all navigators structure. Usually, we'd render this component at the root of our app, which is usually the component exported from ",
+        course1: 'HTML',
+        course2: "CSS",
+        Course3: "JavaScript",
+    },
+    {
+        id: "5",
+        title: "Web Development",
+        image: require('../../assets/a.jpg'),
+        discription:
+            "This component must wrap all navigators structure. Usually, we'd render this component at the root of our app, which is usually the component exported from ",
+        course1: 'HTML',
+        course2: "CSS",
+        Course3: "JavaScript",
+    },
+    {
+        id: "6",
+        title: "Web Development",
+        image: require('../../assets/a.jpg'),
+        discription:
+            "This component must wrap all navigators structure. Usually, we'd render this component at the root of our app, which is usually the component exported from ",
+        course1: 'HTML',
+        course2: "CSS",
+        Course3: "JavaScript",
+    },
+];
+
+export default Courses;
